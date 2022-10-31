0858d0f468f2a0ce03290c2b3dae0b04628c8ecf
Charles <charles.saternos@gmail.com>
2022-10-30 20:43:02 -0400

    make release.py portable

--- release.py
+++ release.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import glob
+import zipfile
 parser = argparse.ArgumentParser()
 
 parser.add_argument('version')
@@ -11,6 +13,7 @@ if not args.skip_build:
     os.system('python -m nuitka --onefile game.py')
 
 dirname = f'hydroplane-{args.version}'
-os.mkdir(dirname)
-os.system(f'cp *.glb *.ogg *.wav *.png *.vert *.frag game.bin {dirname}')
-os.system(f'zip {dirname}.zip -r {dirname}')
+files = sum([glob.glob(p) for p in '*.glb *.ogg *.wav *.png *.vert *.frag game.bin'.split()], [])
+with zipfile.ZipFile(f'{dirname}.zip', 'w') as zipf:
+    for f in files:
+        zipf.write(f, arcname=f'{dirname}/{f}')
